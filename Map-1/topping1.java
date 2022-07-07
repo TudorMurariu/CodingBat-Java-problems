97e119535977a1fbec84e428278ab718a5d35ada
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-07-07 17:02:43 +0300

    Create topping1.java

---
+++ Map-1/topping1.java
@@ -0,0 +1,6 @@
+public Map<String, String> topping1(Map<String, String> map) {
+  map.put("bread","butter");
+  if(map.containsKey("ice cream"))
+    map.put("ice cream","cherry");
+  return map;
+}
